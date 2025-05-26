ee3d4f964210623e2ec7c853cbe10001a2cb600d
Aggrey Lelei <aggreykiplimo@gmail.com>
2025-05-26 10:28:13 +0300

    Initial commit

---
+++ src/main/java/com/distributedx/DistributedXApplication.java
@@ -0,0 +1,13 @@
+package com.distributedx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedXApplication.class, args);
+    }
+
+}
