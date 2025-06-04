1284096f4740276448bc6391a933265cd799cb0e
Aggrey Lelei <aggreykiplimo@gmail.com>
2025-06-04 12:11:30 +0300

    Request object that carries the necessary data to create a passenger

---
+++ src/services/user/src/main/java/com/distributedx/user/users/features/createuser/CreateUserCommand.java
@@ -0,0 +1,18 @@
+package com.distributedx.user.users.features.createuser;
+import java.util.UUID;
+import buildingblocks.core.event.InternalCommand;
+import buildingblocks.mediator.abstractions.commands.ICommand;
+import com.distributedx.user.users.dtos.UserDto;
+
+
+public record CreateUserCommand(
+        UUID id,
+        String userName,
+        String email,
+        String password,
+        String fullName
+) implements ICommand<UserDto>, InternalCommand {
+    public CreateUserCommand {
+    }
+}
+
