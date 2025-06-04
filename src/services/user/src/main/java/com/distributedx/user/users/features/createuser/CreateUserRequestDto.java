fd57a915a72873a733c79e99aae92ac77a1e7c91
Aggrey Lelei <aggreykiplimo@gmail.com>
2025-06-04 10:52:43 +0300

    Added Dtos functionality for data transfer

---
+++ src/services/user/src/main/java/com/distributedx/user/users/features/createuser/CreateUserRequestDto.java
@@ -0,0 +1,10 @@
+package com.distributedx.user.users.features.createuser;
+
+public record CreateUserRequestDto(
+        String userName,
+        String fullName,
+        String email,
+        String password
+        ){
+}
+
