a35a7f16a75d24cda22964209e7d3b285794f271
Aggrey Lelei <aggreykiplimo@gmail.com>
2025-07-03 16:11:29 +0300

    feat(persistence): add init-db.sql for database initialization

---
+++ deployments/postgres-init/init-db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE user_write;
+CREATE DATABASE keycloak;
+-- Grant privileges if necessary, though the default 'postgres' user should have them
+-- For example:
+-- GRANT ALL PRIVILEGES ON DATABASE passenger_write TO postgres;
