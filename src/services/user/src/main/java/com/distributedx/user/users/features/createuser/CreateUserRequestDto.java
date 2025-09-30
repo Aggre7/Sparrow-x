d576c4445edf2a34cd5c7ad1553c1f0431718c11
Aggrey Lelei <148534652+Aggre7@users.noreply.github.com>
2025-09-30 21:14:12 +0300

    Delete src directory

--- src/services/user/src/main/java/com/distributedx/user/users/features/createuser/CreateUserRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.distributedx.user.users.features.createuser;
-
-public record CreateUserRequestDto(
-        String userName,
-        String fullName,
-        String email,
-        String password){
-}
-
